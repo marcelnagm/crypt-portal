57c73af40b69956edf5f757b6f217324b5f26def
Marcel Nagm <marcel.nagm@gmail.com>
2020-12-16 17:55:51 -0400

    REsolvido initial commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,4 @@
+<h1>{{$now}}</h1>
+
+<h2>Message: {{$message}}</h2>
+<h2>Id: {{$id}}</h2>
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::resource('produtos', 'App\Http\Controllers\ProdutosController');
+
+//Route::prefix('produtos')->group(function () {
+//    Route::get('/new', 'App\Http\Controllers\ProdutosController@create');
+//
+//    Route::post('/store', 'App\Http\Controllers\ProdutosController@create')->name('produtos.store');
+//    
+//});
+
+//Route::prefix('produtos')->group(function () {
+//    Route::get('produtos/new', 'ProdutosController@create');
+//});
+
+
+//Route::get('/greeting', function () {
+//    return 'Hello World';
+//});
+
+//Route::match(['get', 'post'], '/', function () {
+//    //
+//});
+
+//Route::redirect('/here', '/there');
+
+
+//Route::view('/welcome2/{id}', 'test', ['now' => 'Pega','message' => 'vamos que vamos']);
+
+//Route::get(
+//    '/user/profile',
+//    [UserProfileController::class, 'show']
+//)->name('profile');
+
+// ideal para modulos
+//Route::prefix('admin')->group(function () {
+//    Route::get('/users', function () {
+//        // Matches The "/admin/users" URL
+//    });
+//});
