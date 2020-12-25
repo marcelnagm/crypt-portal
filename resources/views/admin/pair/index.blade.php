cb52aa3da31c76af4db300567900550a573453ae
helmiton.filho <helmiton.filho@gmail.com>
2020-12-25 09:41:24 -0500

    Ajustes tela pares

--- resources/views/admin/pair/index.blade.php
+++ resources/views/admin/pair/index.blade.php
@@ -1,36 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
         <div class="row">
             @include('admin.sidebar')
 
-            <div class="col-md-9">
+            <div class="col-md-12 col-lg-12 col-xl-6 d-block mx-auto">
                 <div class="card">
-                    <div class="card-header">Pair</div>
+                    <div class="card-header">Pairs</div>
                     <div class="card-body">
-                        <a href="{{ url('/admin/pair/create') }}" class="btn btn-success btn-sm" title="Add New Pair">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
-                        </a>
-                        <a href="{{ url('/admin/pair_generate') }}" class="btn btn-success btn-sm" title="Retrive From Binance">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Retrive From Binance
-                        </a>
-
-                        <form method="GET" action="{{ url('/admin/pair') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
-                                <span class="input-group-append">
-                                    <button class="btn btn-secondary" type="submit">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </form>
-
-                        <br/>
-                        <br/>
+                        <p>
+                          <a href="{{ url('/admin/pair/create') }}" class="btn btn-success btn-sm" title="Add New Pair">
+                              <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                          </a>
+                          <a href="{{ url('/admin/pair_generate') }}" class="btn btn-success btn-sm" title="Retrive From Binance">
+                              <i class="fa fa-plus" aria-hidden="true"></i> Retrive From Binance
+                          </a>
+                        </p>
                         <div class="table-responsive">
-                            <table class="table">
+                            <table id="example-1" class="table">
                                 <thead>
                                     <tr>
                                         <th>#</th><th>Pair</th><th>Main Coin</th><th>Sec Coin</th><th>Actions</th>
@@ -55,12 +42,10 @@
                                 @endforeach
                                 </tbody>
                             </table>
-                            <div class="pagination-wrapper"> {!! $pair->appends(['search' => Request::get('search')])->render() !!} </div>
                         </div>
 
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
--- resources/views/user/configuration/edit.blade.php
+++ resources/views/user/configuration/edit.blade.php
@@ -1,11 +1,10 @@
    @extends('layout_user')
 
 @section('content')
-    <div class="container">
         <div class="row">
             @include('admin.sidebar')
 
-            <div class="col-md-9">
+            <div class="col-md-12 col-lg-12 col-xl-6 d-block mx-auto">
                 <div class="card">
                     <div class="card-header">Edit Configuration #{{ $configuration->id }}</div>
                     <div class="card-body">                                                
@@ -29,5 +28,4 @@
                 </div>
             </div>
         </div>
-    </div>
 @endsection
