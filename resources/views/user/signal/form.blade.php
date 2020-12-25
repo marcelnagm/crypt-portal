485979833aeb2c5a66a877e57f6eb46d82d9a77c
helmiton.filho <helmiton.filho@gmail.com>
2020-12-25 11:02:17 -0500

    Ajustes telas signal create

--- resources/views/user/signal/form.blade.php
+++ resources/views/user/signal/form.blade.php
@@ -18,28 +18,28 @@
 </div>
 <div class="form-group {{ $errors->has('target_1') ? 'has-error' : ''}}">
     <label for="target_1" class="control-label">{{ 'Target 1' }}</label>
-    <input class="form-control" name="target_1_p" type="number" id="target_1" value="{{ isset($signal->target_1_p) ? $signal->target_1_p : ''}}" >%
+    <input class="form-control" name="target_1_p" type="number" id="target_1" value="{{ isset($signal->target_1_p) ? $signal->target_1_p : ''}}" placeholder="%">
     {!! $errors->first('target', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('target_2') ? 'has-error' : ''}}">
     <label for="target_2" class="control-label">{{ 'Target 2' }}</label>
-    <input class="form-control" name="target_2_p" type="number" id="target_2" value="{{ isset($signal->target_2_p) ? $signal->target_2_p : ''}}" >%
+    <input class="form-control" name="target_2_p" type="number" id="target_2" value="{{ isset($signal->target_2_p) ? $signal->target_2_p : ''}}" placeholder="%">
     {!! $errors->first('target', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('target_3') ? 'has-error' : ''}}">
     <label for="target_3" class="control-label">{{ 'Target 3' }}</label>
-    <input class="form-control" name="target_3_p" type="number" id="target_3" value="{{ isset($signal->target_3_p) ? $signal->target_3_p : ''}}" >%
+    <input class="form-control" name="target_3_p" type="number" id="target_3" value="{{ isset($signal->target_3_p) ? $signal->target_3_p : ''}}" placeholder="%">
     {!! $errors->first('target', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('stop') ? 'has-error' : ''}}">
     <label for="stop" class="control-label">{{ 'Stop' }}</label>
-    <input class="form-control" name="stop_p" type="number" id="stop" value="{{ isset($signal->stop_p) ? $signal->stop_p : ''}}" >%
+    <input class="form-control" name="stop_p" type="number" id="stop" value="{{ isset($signal->stop_p) ? $signal->stop_p : ''}}" placeholder="%">
     {!! $errors->first('stop', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('stop_up') ? 'has-error' : ''}}">
     <label for="stop_up" class="control-label">{{ 'Stop Up' }}</label>
-    <input class="form-control" name="stop_up" type="string" id="stop_up" value="{{ isset($signal->stop_up) ? $signal->stop_up : ''}}" >%
+    <input class="form-control" name="stop_up" type="string" id="stop_up" value="{{ isset($signal->stop_up) ? $signal->stop_up : ''}}" placeholder="%">
     {!! $errors->first('stop_up', '<p class="help-block">:message</p>') !!}
 </div>
 
